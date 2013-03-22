83fdd802d706905aef3f4a33123315963a9ab1db
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2013-03-22 15:37:58 -0700

    initial infrastructure

---
+++ configuration.py
@@ -0,0 +1,24 @@
+from supy.defaults import *
+
+def mainTree() :
+    return ('/','NSWHitsTree')
+
+def leavesToBlackList() :
+    #return ['L1triggered', 'triggered', 'DCSBits']
+    return []
+
+def cppROOTDictionariesToGenerate() :
+    return [
+        #("vector<ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> > >", "vector;Math/LorentzVector.h"),
+        #ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiM4D<float> > etc. is addressed in linkdef.cxx
+        ("ROOT::Math::Cartesian3D<float>", "Math/Point3D.h"),
+        ("ROOT::Math::DisplacementVector3D<ROOT::Math::Cartesian3D<float>,ROOT::Math::DefaultCoordinateSystemTag>", "Math/Vector3D.h"),
+        ("vector<ROOT::Math::DisplacementVector3D<ROOT::Math::Cartesian3D<float>,ROOT::Math::DefaultCoordinateSystemTag> >", "vector;Math/Vector3D.h"),
+        ("ROOT::Math::PositionVector3D<ROOT::Math::Cartesian3D<float>,ROOT::Math::DefaultCoordinateSystemTag>", "Math/Point3D.h"),
+        # for now we don't have any vec<vec< >>
+        ]
+
+def cppFiles() :
+    return [
+            "cpp/linkdef.cxx",
+            ]
