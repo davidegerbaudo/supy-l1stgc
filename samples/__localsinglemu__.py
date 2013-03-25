bfd4ea42224748f5cf1a4a1b53ad755d6b6ad085
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2013-03-24 21:44:07 -0700

    add jochen ntuples

--- samples/__localsinglemu__.py
+++ samples/__localsinglemu__.py
@@ -2,3 +2,9 @@ from supy.samples import SampleHolder
 localsinglemu = SampleHolder()
 
 localsinglemu.add("SingleMu", '["/tmp/MuonEFlat25to200GeV_17_5k_v1.root"]', lumi=1.0e-5) # /pb
+
+
+localsinglemu.add('JochenSingleMu',
+                  '['+','.join(['"/tmp/atlasG4.%seta_v1NSWHitsDumpAlg.root"'%pn
+                                for pn in ['pos','neg']])+']',
+                  lumi=1.0e-5)
