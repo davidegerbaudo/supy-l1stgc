c6fb93e24299033ace80d8ecf403b6f89942c33f
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2013-03-25 12:00:13 -0700

    add comment: file origin

--- samples/__localsinglemu__.py
+++ samples/__localsinglemu__.py
@@ -3,7 +3,8 @@ localsinglemu = SampleHolder()
 
 localsinglemu.add("SingleMu", '["/tmp/MuonEFlat25to200GeV_17_5k_v1.root"]', lumi=1.0e-5) # /pb
 
-
+# Two files from
+# /afs/cern.ch/user/d/daits/workarea/public/nsw/rel_1/atlasG4.*eta_v1NSWHitsDumpAlg.root
 localsinglemu.add('JochenSingleMu',
                   '['+','.join(['"/tmp/atlasG4.%seta_v1NSWHitsDumpAlg.root"'%pn
                                 for pn in ['pos','neg']])+']',
