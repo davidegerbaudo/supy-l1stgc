36253fbd2bae5616e7aee1d5db6cc08ef2819ff9
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2013-03-25 16:02:59 -0700

    fix unit

--- steps/histos.py
+++ steps/histos.py
@@ -7,7 +7,7 @@ class xyMap(analysisStep) :
     def __init__(self, coll='',indices='', index = None,nX=500,xLo=-5000.0,xHi=+5000.0,nY=500,yLo=-5000.0,yHi=+5000.0,title='') :
         for item in ['coll','indices','index','nX','xLo','xHi','nY','yLo','yHi','title'] : setattr(self,item,eval(item))
         self.moreName = coll + ("[i[%d]]:%s"%(index,indices) if index!=None else "")
-        self.title = "%s;x [cm]; y [cm]"%(title if title else coll + ("[%s%s]"%(indices, "[%d]"%index if index!=None else "") if indices else ""))
+        self.title = "%s;x [mm]; y [mm]"%(title if title else coll + ("[%s%s]"%(indices, "[%d]"%index if index!=None else "") if indices else ""))
     def uponAcceptance(self,eventVars) :
         positions = eventVars[self.coll]
         if positions is None : return
