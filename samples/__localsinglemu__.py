3c0a0ab1d18aedf6318ff339d1614a7b00f6163e
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2013-03-25 15:49:32 -0700

    add separate pos/neg definitions

--- samples/__localsinglemu__.py
+++ samples/__localsinglemu__.py
@@ -5,7 +5,17 @@ localsinglemu.add("SingleMu", '["/tmp/MuonEFlat25to200GeV_17_5k_v1.root"]', lumi
 
 # Two files from
 # /afs/cern.ch/user/d/daits/workarea/public/nsw/rel_1/atlasG4.*eta_v1NSWHitsDumpAlg.root
+poseneg = ['pos','neg']
 localsinglemu.add('JochenSingleMu',
                   '['+','.join(['"/tmp/atlasG4.%seta_v1NSWHitsDumpAlg.root"'%pn
-                                for pn in ['pos','neg']])+']',
+                                for pn in poseneg])+']',
+                  lumi=1.0e-5)
+
+localsinglemu.add('JochenSingleMuPos',
+                  '['+','.join(['"/tmp/atlasG4.%seta_v1NSWHitsDumpAlg.root"'%pn
+                                for pn in poseneg[:1]])+']',
+                  lumi=1.0e-5)
+localsinglemu.add('JochenSingleMuNeg',
+                  '['+','.join(['"/tmp/atlasG4.%seta_v1NSWHitsDumpAlg.root"'%pn
+                                for pn in poseneg[1:]])+']',
                   lumi=1.0e-5)
