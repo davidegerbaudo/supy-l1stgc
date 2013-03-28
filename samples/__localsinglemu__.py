737d46b316d76ffce8b789896bdb0dd4065e2929
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2013-03-27 19:34:21 -0700

    switch to todays samples

--- samples/__localsinglemu__.py
+++ samples/__localsinglemu__.py
@@ -4,18 +4,16 @@ localsinglemu = SampleHolder()
 localsinglemu.add("SingleMu", '["/tmp/MuonEFlat25to200GeV_17_5k_v1.root"]', lumi=1.0e-5) # /pb
 
 # Two files from
-# /afs/cern.ch/user/d/daits/workarea/public/nsw/rel_1/atlasG4.*eta_v1NSWHitsDumpAlg.root
+# /eos/atlas/user/g/gerbaudo/trigger/nsw/2013-03-27/atlasG4.pos.NSWHitsDumper.root
+# (produced from ~jomeyer/public/NSW/atlasG4.*.hits.pool.root)
 poseneg = ['pos','neg']
 localsinglemu.add('JochenSingleMu',
-                  '['+','.join(['"/tmp/atlasG4.%seta_v1NSWHitsDumpAlg.root"'%pn
-                                for pn in poseneg])+']',
+                  '['+','.join(['"/tmp/atlasG4.%s.NSWHitsDumper.root"'%pn for pn in poseneg])+']',
                   lumi=1.0e-5)
 
 localsinglemu.add('JochenSingleMuPos',
-                  '['+','.join(['"/tmp/atlasG4.%seta_v1NSWHitsDumpAlg.root"'%pn
-                                for pn in poseneg[:1]])+']',
+                  '['+','.join(['"/tmp/atlasG4.%s.NSWHitsDumper.root"'%pn for pn in poseneg[:1]])+']',
                   lumi=1.0e-5)
 localsinglemu.add('JochenSingleMuNeg',
-                  '['+','.join(['"/tmp/atlasG4.%seta_v1NSWHitsDumpAlg.root"'%pn
-                                for pn in poseneg[1:]])+']',
+                  '['+','.join(['"/tmp/atlasG4.%s.NSWHitsDumper.root"'%pn for pn in poseneg[1:]])+']',
                   lumi=1.0e-5)
