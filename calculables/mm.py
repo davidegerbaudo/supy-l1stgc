5ff7f41e460b95bd9f2ad19e61c6915d984da0f6
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2013-03-28 16:14:45 -0700

    move comments to mm, where they belong

---
+++ calculables/mm.py
@@ -0,0 +1,26 @@
+
+
+
+#
+# todo : define calculables for micromega hits
+#
+# -- collection = ('Hits_MM_','')
+# -- stashVars = [
+# -- 'n',
+# -- 'globalTime',
+# -- 'globalPositionX',
+# -- 'globalPositionY',
+# -- 'globalPositionZ',
+# -- 'globalDirectionX',
+# -- 'globalDirectionY',
+# -- 'globalDirectionZ',
+# --
+# -- 'localPositionX',
+# -- 'localPositionY',
+# -- 'localPositionZ',
+# --
+# -- 'particleEncoding',
+# -- 'kineticEnergy',
+# -- 'depositEnergy',
+# -- 'StepLength',
+# -- ]
--- calculables/stgc.py
+++ calculables/stgc.py
@@ -271,28 +271,3 @@ class Any4LayersWedgeTrigger(wrappedChain.calculable) :
     def update(self, _) :
         self.value = any(self.source[self.WedgesWith4ActiveLayers])
 #__________________________________________________________
-
-
-#
-# todo : define calculables for micromega hits
-#
-# -- collection = ('Hits_MM_','')
-# -- stashVars = [
-# -- 'n',
-# -- 'globalTime',
-# -- 'globalPositionX',
-# -- 'globalPositionY',
-# -- 'globalPositionZ',
-# -- 'globalDirectionX',
-# -- 'globalDirectionY',
-# -- 'globalDirectionZ',
-# --
-# -- 'localPositionX',
-# -- 'localPositionY',
-# -- 'localPositionZ',
-# --
-# -- 'particleEncoding',
-# -- 'kineticEnergy',
-# -- 'depositEnergy',
-# -- 'StepLength',
-# -- ]
