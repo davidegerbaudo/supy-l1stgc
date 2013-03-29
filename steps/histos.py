d3f129d0257c5d6a0731916b12be79c460c6e3eb
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2013-03-28 22:45:29 -0700

    implement padIndexAvg

--- steps/histos.py
+++ steps/histos.py
@@ -1,6 +1,6 @@
 #import copy,array,os,collections
-#import ROOT as r
-from supy import steps
+import ROOT as r
+from supy import steps, analysisStep
 from math import pi
 
 #___________________________________________________________
@@ -30,3 +30,41 @@ class phiVsTheta(steps.histos.value):
         super(phiVsTheta, self).__init__(var, N, lo, hi, indices, index, title, w)
     def wrapName(self) : return ('.theta', '.phi')
     def wrap(self, val) : return (val[0].theta(), val[1].phi())
+#___________________________________________________________
+class padIndexAvg(analysisStep) :
+    "Plot the pad index average(ieta or iphi)"
+    def __init__(self, coll, indices='', ieta=False, iphi=False,
+                 N=(100,100),lo=(-5000.0,-5000.0),hi=(+5000.0,+5000.0),
+                 title='') :
+        assert ieta!=iphi, "Specify either ieta or iphi"
+        for item in ['coll','indices','ieta','iphi','N','lo','hi','title'] : setattr(self,item,eval(item))
+        self.hname = '_'.join(coll)+'padIndexAvg'+('iEta' if ieta else 'iPhi')+indices
+        self.cumname = 'cum_'+self.hname
+        self.cntname = 'cnt_'+self.hname
+        self.avgname = 'avg_'+self.hname
+        self.moreName = self.hname
+        if not self.title : self.title = 'average '+('iEta' if ieta else 'iPhi')+indices
+    def uponAcceptance(self, eventVars) :
+        positions = eventVars['Pos'.join(self.coll)]
+        etaphiIds = eventVars['PadLocalIndices'.join(self.coll)]
+        indices   = eventVars[self.indices]
+        for i in indices :
+            pos, etaphi = positions[i], etaphiIds[i]
+            self.book.fill((pos.x(), pos.y()),
+                           self.cumname, self.N, self.lo, self.hi, title=self.cumname,
+                           w=(etaphi[0] if self.ieta else etaphi[1]))
+            self.book.fill((pos.x(), pos.y()),
+                           self.cntname, self.N, self.lo, self.hi, title=self.cntname)
+    def mergeFunc(self, products) :
+        num = r.gDirectory.Get(self.cumname)
+        den = r.gDirectory.Get(self.cntname)
+        if not num : return
+        if not den : return
+        eff = num.Clone('avg'+self.cumname)
+        if not eff : return
+        eff.SetTitle(self.title)
+        eff.Divide(num,den,1,1,"B")
+        # for bin in [0,self.N+1] :
+        #     eff.SetBinContent(bin,0)
+        #     eff.SetBinError(bin,0)
+        eff.Write()
