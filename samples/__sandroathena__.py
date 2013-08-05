7f725c5180d7fcf430908f2dd1d360eaf276a885
gerbaudo <davide.gerbaudo@gmail.com>
2013-08-06 00:33:24 +0200

    add full-stats files

--- samples/__sandroathena__.py
+++ samples/__sandroathena__.py
@@ -4,3 +4,8 @@ sandroathena = SampleHolder()
 # get the test file from Sandro:
 # cp /afs/cern.ch/work/d/dimattia/public/LVL1NSW/PadIndexValidation/NSWL1Simulation.root /tmp/
 sandroathena.add("Athena", '["/tmp/NSWL1Simulation.root"]', lumi=1.0e-5) # /pb
+
+basedir = '/afs/cern.ch/user/g/gerbaudo/work/public/nsw/batch_digit/output-2013-08-05/'
+sandroathena.add("Athena50k",
+                 'utils.fileListFromDisk(location = "%s/*.root", isDirectory = False)'%basedir,
+                 lumi=1.0e-5)
