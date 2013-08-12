4c79096c4aaa99acde8486003f231bd2500cf7a0
gerbaudo <davide.gerbaudo@gmail.com>
2013-08-12 10:30:55 +0200

    latest  ntuple (with dummy val for unconverted idx)

--- samples/__sandroathena__.py
+++ samples/__sandroathena__.py
@@ -3,9 +3,11 @@ sandroathena = SampleHolder()
 
 # get the test file from Sandro:
 # cp /afs/cern.ch/work/d/dimattia/public/LVL1NSW/PadIndexValidation/NSWL1Simulation.root /tmp/
-sandroathena.add("Athena", '["/tmp/NSWL1Simulation.root"]', lumi=1.0e-5) # /pb
+sandroathena.add("Athena",
+                 '["/tmp/NSWL1Simulation.root"]',
+                 lumi=1.0e-5) # /pb
 
-basedir = '/afs/cern.ch/user/g/gerbaudo/work/public/nsw/batch_digit/output-2013-08-05/'
+basedir = '/afs/cern.ch/user/g/gerbaudo/work/public/nsw/batch_digit/output-2013-08-11/'
 sandroathena.add("Athena50k",
                  'utils.fileListFromDisk(location = "%s/*.root", isDirectory = False)'%basedir,
                  lumi=1.0e-5)
